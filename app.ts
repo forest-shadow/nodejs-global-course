84ef3740f7c994850915f13f2cbc4f5a62de106f
Ivan Arevkov <ivan.arevkov@gmail.com>
2021-03-20 20:18:13 +0300

    switch to eslint from tslint & update packages

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 interface TestInterface {
-    foo: string
-    bar: string
+  foo: string
+  bar: string
 }
 
 class TestClass implements TestInterface {
-    public foo = 'foo'
-    public bar = 'bar'
+  public foo = 'foo'
+  public bar = 'bar'
 }
+
+const test = new TestClass()
+console.log(test.foo)
